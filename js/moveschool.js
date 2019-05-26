c7e2b717cc6cb64c910888bbc9441ae93e7d3532
jwj8228 <jiaowenjun1994@vip.qq.com>
2019-05-26 19:03:11 +0800

    修改文件名

--- js/moveschool.js
+++ js/moveschool.js
@@ -1,21 +1,22 @@
 // 展示一
-(function(){
+(function () {
 	var $content = $("#content"),
 		$presentation1 = $(".presentation1");
 	gainHeight();
-	$(window).resize(function(){
+	$(window).resize(function () {
 		gainHeight();
 	})
-	function gainHeight(){
+
+	function gainHeight() {
 		var windH = $(window).height(),
 			h = windH - parseFloat($content.css("margin"));
 		$presentation1.height(h)
 	}
 }());
 //展示三
-(function(){
+(function () {
 	var $presentation3 = $("#content .presentation3")
-		$view = $presentation3.find(".view"),
+	$view = $presentation3.find(".view"),
 		$viewLi = $view.find("li"),
 		$item = $presentation3.find(".item"),
 		$itemLi = $item.find("li"),
@@ -23,40 +24,41 @@
 		index = 0,
 		timer = null;
 	autoPlay();
-	$item.hover(function(){
+	$item.hover(function () {
 		clearInterval(timer);
 	}, autoPlay);
-	$view.hover(function(){
+	$view.hover(function () {
 		clearInterval(timer);
-	}, autoPlay); 
-	$itemLi.each(function(){
-		$(this).mouseenter(function(){
+	}, autoPlay);
+	$itemLi.each(function () {
+		$(this).mouseenter(function () {
 			var i = $(this).index();
 			$(this).addClass("on").siblings().removeClass("on");
 			$viewLi.eq(i).addClass("on").siblings().removeClass("on");
 			index = i;
 		})
 	});
-	function autoPlay(){
-		timer = setInterval(function(){
+
+	function autoPlay() {
+		timer = setInterval(function () {
 			index++;
 			index %= len;
 			$itemLi.eq(index).addClass("on").siblings().removeClass("on");
 			$viewLi.eq(index).addClass("on").siblings().removeClass("on");
-		},2000)
+		}, 2000)
 	}
 })();
 //btn定位
-(function(){
+(function () {
 	var $content = $("#content"),
 		$part = $content.find(".part"),
 		$btn = $("#content .btn");
-	$btn.click(function(){
+	$btn.click(function () {
 		var index = $btn.index($(this)),
-			scrollT = $part.eq(index + 1).offset().top - ($(window).height()-$part.eq(index+1).height() + 70)/2;
-		console.log(scrollT,index)
+			scrollT = $part.eq(index + 1).offset().top - ($(window).height() - $part.eq(index + 1).height() + 70) / 2;
+		console.log(scrollT, index)
 		$("body,html").animate({
-            scrollTop : scrollT
-        },800);
-	})	
+			scrollTop: scrollT
+		}, 800);
+	})
 })();
