274c5f5ca5ac12c9b3a56d4d3ab981e6c0e30ea2
jwj8228 <jiaowenjun1994@vip.qq.com>
2019-05-26 18:35:51 +0800

    二次修改

--- js/index.js
+++ js/index.js
@@ -1,83 +1,70 @@
 // 内容banner
 (function () {
-    var num = 0;
+    bannerUl = $(".banner").find("ul"),
+        bannerLi = bannerUl.find("li"),
+        tab = $(".banner").find("span"),
+        len = bannerLi.length,
+        timer = null,
+        index = 0;
+    bannerLi.eq(index).stop().fadeIn().siblings().stop().fadeOut();
+    autoPlay();
 
-    function banner() {
-        $('.but li').eq(num).addClass('active').siblings().removeClass('active');
-        $('.bg').css({
-            left: -num * $('.box').width()
-        });
+    function autoPlay() {
+        timer = setInterval(function () {
+            index++;
+            index = index > len - 1 ? 0 : index;
+            bannerLi.eq(index).stop().fadeIn().siblings().stop().fadeOut();
+            tab.eq(index).addClass("on").siblings().removeClass("on");
+        }, 3000);
     }
-
-    function next() {
-        num++;
-        if (num > $('.but li').length - 1) {
-            num = 0;
-        };
-        banner();
-    }
-    $('.right').click(next);
-    $('.left').click(function () {
-        num--;
-        if (num < 0) {
-            num = $('.but li').length - 1;
-        };
-        banner();
+    tab.each(function () {
+        $(this).click(function () {
+            $(this).addClass("on").siblings().removeClass("on");
+            index = $(this).index();
+            bannerLi.eq(index).stop().fadeIn().siblings().stop().fadeOut();
+        })
     })
-    $('.but li').click(function () {
-        num = $(this).index();
-        banner();
-    })
-    var time = setInterval(next, 1500);
-    // 划上清除定时器 划出启动
-    $('.banner').hover(
-        function () {
-            clearInterval(time);
-        },
-        function () {
-            time = setInterval(next, 1500);
-        }
-    );
+    $(".banner").hover(function () {
+        clearInterval(timer);
+    }, autoPlay)
 })();
 // 名师风采
 (function () {
-    var $ul = $('.case ul'),
-        $li = $ul.children(),
-        $btn = $('.case .btn'),
-        width = $ul.children().eq(0).width(),
-        length = $li.length,
+    li = $('.case ul').children(),
+        width = $('.case ul').children().eq(0).width(),
+        length = li.length,
         midIndex = Math.floor(length / 2),
         clickTime = 0,
         sumWidth = 0,
         timer;
     changeClassName();
     setTimeout(function () {
-        sumWidth = $li.last()[0].offsetLeft + $li.last().width();
-        $ul.css('marginLeft', -sumWidth / 2 + 'px').css('opacity', 1);
+        sumWidth = li.last()[0].offsetLeft + li.last().width();
+        $('.case ul').css('marginLeft', -sumWidth / 2 + 'px').css('opacity', 1);
     }, 300);
     $(window).resize(function () {
         clearTimeout(timer);
         timer = setTimeout(function () {
-            sumWidth = $li.last()[0].offsetLeft + $li.last().width();
-            $ul.animate({
+            sumWidth = li.last()[0].offsetLeft + li.last().width();
+            $('.case ul').animate({
                 'marginLeft': -sumWidth / 2 + 'px'
             }, 300);
         }, 300);
     });
-    $btn.click(function () {
+    $('.case .btn').click(function () {
         if (new Date() - clickTime > 350) {
-            var index = $btn.index($(this));
+            var index = $('.case .btn').index($(this));
             if (index) {
                 midIndex++;
                 midIndex %= length;
-                $ul.stop().animate({
+                $('.case ul').stop().animate({
                     'marginLeft': -sumWidth / 2 - width + 'px'
                 }, 300, function () {
                     $(this).css('marginLeft', -sumWidth / 2 + 'px').append($(this).children().first());
                 });
             } else {
                 midIndex = --midIndex < 0 ? length - 1 : midIndex--;
-                $ul.stop().animate({
+                $('.case ul').stop().animate({
                     'marginLeft': -sumWidth / 2 + width + 'px'
                 }, 300, function () {
                     $(this).css('marginLeft', -sumWidth / 2 + 'px').prepend($(this).children().last());
@@ -91,9 +78,9 @@
     function changeClassName() {
         var leftIndex = midIndex - 1 < 0 ? length - 1 : midIndex - 1,
             rightIndex = midIndex + 1 >= length ? 0 : midIndex + 1;
-        $li.removeClass('mid slide').css("margin", "0");
-        $li.eq(midIndex).addClass('mid').css("margin", "0px 0px 0px 1px");
-        $li.eq(leftIndex).addClass('slide').css("marginLeft", "25px");
-        $li.eq(rightIndex).addClass('slide').css("marginRight", "25px");
+        li.removeClass('mid slide').css("margin", "0");
+        li.eq(midIndex).addClass('mid').css("margin", "0px 0px 0px 1px");
+        li.eq(leftIndex).addClass('slide').css("marginLeft", "25px");
+        li.eq(rightIndex).addClass('slide').css("marginRight", "25px");
     }
 })();
--- js/list.js
+++ js/list.js
@@ -1,7 +1,7 @@
 // 头部导航栏二级菜单
-(function(){
+(function () {
 	var $more = $("#header .more");
-	$more.click(function(){
+	$more.click(function () {
 		$(this).find(".nav-second").toggle();
-    })
+	})
 })();
