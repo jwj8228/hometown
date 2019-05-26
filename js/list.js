d1e4f2d467cdf9d059fb8bf2cc04689eaf44ac94
jwj8228 <jiaowenjun1994@vip.qq.com>
2019-05-26 19:24:28 +0800

    修改页面2的部分动态效果

--- js/list.js
+++ js/list.js
@@ -1,7 +1,6 @@
 // 头部导航栏二级菜单
 (function () {
-	var $more = $("#header .more");
-	$more.click(function () {
+	$("#header .more").click(function () {
 		$(this).find(".nav-second").toggle();
 	})
 })();
--- js/moveschool.js
+++ js/moveschool.js
@@ -1,7 +1,5 @@
 // 展示一
 (function () {
-	var $content = $("#content"),
-		$presentation1 = $(".presentation1");
 	gainHeight();
 	$(window).resize(function () {
 		gainHeight();
@@ -9,32 +7,31 @@
 
 	function gainHeight() {
 		var windH = $(window).height(),
-			h = windH - parseFloat($content.css("margin"));
-		$presentation1.height(h)
+			h = windH - parseFloat($("#content").css("margin"));
+		$(".presentation1").height(h)
 	}
 }());
 //展示三
 (function () {
-	var $presentation3 = $("#content .presentation3")
-	$view = $presentation3.find(".view"),
-		$viewLi = $view.find("li"),
-		$item = $presentation3.find(".item"),
-		$itemLi = $item.find("li"),
-		len = $itemLi.length,
+	view = $("#content .presentation3").find(".view"),
+		viewLi = view.find("li"),
+		item = $("#content .presentation3").find(".item"),
+		itemLi = item.find("li"),
+		len = itemLi.length,
 		index = 0,
 		timer = null;
 	autoPlay();
-	$item.hover(function () {
+	item.hover(function () {
 		clearInterval(timer);
 	}, autoPlay);
-	$view.hover(function () {
+	view.hover(function () {
 		clearInterval(timer);
 	}, autoPlay);
-	$itemLi.each(function () {
+	itemLi.each(function () {
 		$(this).mouseenter(function () {
 			var i = $(this).index();
 			$(this).addClass("on").siblings().removeClass("on");
-			$viewLi.eq(i).addClass("on").siblings().removeClass("on");
+			viewLi.eq(i).addClass("on").siblings().removeClass("on");
 			index = i;
 		})
 	});
@@ -43,22 +40,20 @@
 		timer = setInterval(function () {
 			index++;
 			index %= len;
-			$itemLi.eq(index).addClass("on").siblings().removeClass("on");
-			$viewLi.eq(index).addClass("on").siblings().removeClass("on");
+			itemLi.eq(index).addClass("on").siblings().removeClass("on");
+			viewLi.eq(index).addClass("on").siblings().removeClass("on");
 		}, 2000)
 	}
 })();
-//btn定位
+// btn定位
 (function () {
-	var $content = $("#content"),
-		$part = $content.find(".part"),
-		$btn = $("#content .btn");
-	$btn.click(function () {
-		var index = $btn.index($(this)),
-			scrollT = $part.eq(index + 1).offset().top - ($(window).height() - $part.eq(index + 1).height() + 70) / 2;
-		console.log(scrollT, index)
-		$("body,html").animate({
-			scrollTop: scrollT
-		}, 800);
-	})
+	part = $("#content").find(".part"),
+		$("#content .btn").click(function () {
+			var index = $("#content .btn").index($(this)),
+				scrollT = part.eq(index + 1).offset().top - ($(window).height() - part.eq(index + 1).height() + 70) / 2;
+			console.log(scrollT, index)
+			$("body,html").animate({
+				scrollTop: scrollT
+			}, 800);
+		})
 })();
